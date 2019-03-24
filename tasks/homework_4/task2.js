b0e931fb55f8eb3986f19e8b6f6b242afa6d927d
SallyBons <46354110+SallyBons@users.noreply.github.com>
2019-03-24 14:47:13 +0300

    Update task2.js

--- tasks/homework_4/task2.js
+++ tasks/homework_4/task2.js
@@ -6,12 +6,6 @@ class HallOfFame {
 
         let newArrayLenght = numberOfPlayers;
 
-        // Handle situaton if incomming array of players has bigger length that imcomming length parameter
-        // If we avoid doing so, we will get -1 index for additional element we .push() into players array
-        if (arrayOfPlayers && arrayOfPlayers.length > numberOfPlayers) {
-            newArrayLenght = arrayOfPlayers.length;
-        }
-
         this.listOfPlayers = Array(newArrayLenght).fill('');
 
         // Handle with the imcomming player array if exists and fill the array with new players        
@@ -70,8 +64,6 @@ class HallOfFame {
             delete this.listOfPlayers[index];
             this.listOfPlayers[index] = player;
 
-        } else {
-            console.log(`No more space in the array. We can't add ${player[0]}`);
         }
 
         return this;
@@ -80,15 +72,12 @@ class HallOfFame {
 
 }
 
-// Custom test
-const testCumstomSort = new HallOfFame(3, [['Sam', 10], ["Sad", 10], ["Jesica", 1], ["Micheal", 5]]);
-console.log(testCumstomSort.list);
-testCumstomSort.add(["Monica", 17]);
-console.log(testCumstomSort.list);
+// Additional tests
+const x = new HallOfFame(3, [[ 'Bob', 88 ], [ 'Eva', 66 ], [ 'Ada', 44 ] ]);
+x.add([ 'Clo', 10 ]);
+console.log(x.list);
 
-// Your Test
-const top = new HallOfFame(); // размер 5 по умолчанию
-top.add(["A", 4]).add(["E", 3]).add(["I", 1])
-console.log(top.list) // --> ["A: 4","E: 3","I: 1","",""] //  <-- 2 "пустых игрока" в конце списка
-top.add(["S", 5]).add(["T", 7])
-console.log(top.list) // --> ["T: 7","S: 5","A: 4","E: 3","I: 1"]
+//Additional test #2
+const y = new HallOfFame(3, [[ 'Bob', 88 ], [ 'Kim', 88 ], [ 'Zoe', 88 ] ]);
+y.add([ 'Clo', 10 ]).add([ 'Ada', 44 ]);
+console.log(y.list);
